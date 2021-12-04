68dd3e65e0aefd8381f5db057c36301e1cafbbfc
SChan <smc5407@gmail.com>
2021-12-04 23:17:18 +0900

    소수

---
+++ 기본수학2/소수.py
@@ -0,0 +1,21 @@
+m = int(input())
+n = int(input())
+sum = 0
+minNum = n+1
+
+for i in range(m,n+1):
+    flag = 0
+    for j in range(2,i):
+        if i % j == 0:
+            flag = 1
+            break
+    if flag == 0:
+        if minNum > i:
+            minNum = i
+        sum += i
+
+if sum == 0:
+    print('-1')
+else:
+    print(sum)
+    print(minNum)
