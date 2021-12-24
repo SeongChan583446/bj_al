2eb27efae7ec65e8012e4e98ecccc4936e195f4f
SChan <smc5407@gmail.com>
2021-12-24 23:39:25 +0900

    LCS

---
+++ 동적계획법1/LCS.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+str1 = input().strip().upper()
+str2 = input().strip().upper()
+len1 = len(str1)
+len2 = len(str2)
+print(len1,len2)
+arr = [[0 for _ in range(len1+1)] for _ in range(len2+1)]
+
+for i in range(1,len1+1):
+    for j in range(1,len2+1):
+        print(i,j)
+        if str1[i-1] == str2[j-1]:
+            arr[i][j] = arr[i-1][j-1] + 1
+        else:
+            arr[i][j] = max(arr[i-1][j],arr[i][j-1])
+
+print(arr[-1][-1])
